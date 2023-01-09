fc03472a4f5ad5d3835224f7fc7abdfca5ad01ab
KortextDev <cacortesl01@gmail.com>
2023-01-09 15:10:49 -0500

    Se realizan cambios al index.js, se consulta la API y se agregan elementos al DOM, se eliminan algunos elementos que estaban por defecto

--- src/index.js
+++ src/index.js
@@ -4,3 +4,30 @@
  **/
 
 console.log('Happy hacking :)')
+
+const baseUrl = "https://platzi-avo.vercel.app";
+const URL = "https://platzi-avo.vercel.app/api/avo";
+
+window.fetch(`${baseUrl}/api/avo`).then((res)=>res.json()).then((resjson)=>{
+    const nodeArray = [];
+    const $app = document.getElementById("app");
+    resjson.data.forEach(product => {
+
+        const $image = document.createElement("img");
+        const $title = document.createElement("h2");
+        const $price = document.createElement("p");
+        const $div = document.createElement("div");
+        const image = `${baseUrl}`+product.image;
+        $image.src = image;
+        $title.innerText = product.name;
+        $price.innerText = product.price;
+
+        $div.append($image, $title, $price);
+        nodeArray.push($div);
+    });
+    
+    $app.append(...nodeArray);
+   
+});
+
+
