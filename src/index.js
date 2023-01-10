419eaf8df350402153bcd4f8e497b45beb7a1229
KortexDevLinux <cacortesl01@gmail.com>
2023-01-10 11:49:42 -0500

    Se agregan algunos estilos y lógica paa internalización de los precios

--- src/index.js
+++ src/index.js
@@ -8,19 +8,36 @@ console.log('Happy hacking :)')
 const baseUrl = "https://platzi-avo.vercel.app";
 const URL = "https://platzi-avo.vercel.app/api/avo";
 
+//Internalización
+const formatPrice = (price) =>{
+    //Usamos la API de internalización
+    const newPrice = new window.Intl.NumberFormat("en-EN", {
+        style: "currency",
+        currency: "USD"
+    }).format(price);
+    
+    return newPrice;
+}
+const $app = document.getElementById("app");
+$app.addEventListener("click", (event)=>{
+    console.log(event.target);
+})
+
 window.fetch(`${baseUrl}/api/avo`).then((res)=>res.json()).then((resjson)=>{
     const nodeArray = [];
-    const $app = document.getElementById("app");
+   
     resjson.data.forEach(product => {
 
         const $image = document.createElement("img");
         const $title = document.createElement("h2");
         const $price = document.createElement("p");
+        $title.classList.add("mt-2", "mb-1", "text-red-600");
         const $div = document.createElement("div");
+        $div.classList.add("p-2");
         const image = `${baseUrl}`+product.image;
         $image.src = image;
         $title.innerText = product.name;
-        $price.innerText = product.price;
+        $price.innerText = formatPrice(product.price);
 
         $div.append($image, $title, $price);
         nodeArray.push($div);
